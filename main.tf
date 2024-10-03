390a2f48ba4a0510a57c1761df09fa9898e1943e
Moatez <amairi.moatez777@gmail.com>
2024-10-03 16:01:19 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,3 +24,32 @@ output "owner" {
   description = "The owner of the infrastructure"
   value       = local.owner
 }
+
+
+provider "keycloak" {
+}
+
+resource "keycloak_realm" "realm" {
+  realm   = "my-realm"
+  enabled = true
+}
+
+resource "keycloak_openid_client" "openid_client" {
+  realm_id            = keycloak_realm.realm.id
+  client_id           = "test-client"
+
+  name                = "test client"
+  enabled             = true
+
+  access_type         = "CONFIDENTIAL"
+  valid_redirect_uris = [
+    "http://localhost:8080/openid-callback"
+  ]
+
+  login_theme = "keycloak"
+
+  extra_config = {
+    "key1" = "value1"
+    "key2" = "value2"
+  }
+}
